42e38b0e9234b39609a23e16fd3758fc1dbb213c
Denaid24 <Gamabyta@gmail.com>
2022-06-19 16:05:42 +0300

    New function welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
+import prompt
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f"Hello,{name}!")
 
 def main():
-    print('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__ == '__main__':
     main()
