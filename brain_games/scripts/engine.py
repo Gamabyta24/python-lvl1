4a155e09217ecd864eac266bdb337e449af1cde4
Denaid24 <Gamabyta@gmail.com>
2022-06-24 13:00:25 +0300

    format code

--- brain_games/scripts/engine.py
+++ brain_games/scripts/engine.py
@@ -1,25 +1,24 @@
 import prompt
 
+
 def welcome_user():
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello,{name}!")
 
 
-
-def run(x,cli):
+def run(x, cli):
     cli()
     print(x.make_quest())
     tryes = x.try_count()
-    print('You tries',tryes)
+    print("You tries", tryes)
     for _ in range(tryes):
-        num,right_answer = x.even()
-        print('Question:',num)
-        user_answer = input('Your answer: ')
+        num, right_answer = x.even()
+        print("Question:", num)
+        user_answer = input("Your answer: ")
         if user_answer == right_answer:
-            print('Correct')
+            print("Correct")
         else:
-            print('Try again')
+            print("Try again")
             return
     print("Congrats")
-
--- brain_games/scripts/even_setup.py
+++ brain_games/scripts/even_setup.py
@@ -1,8 +1,10 @@
-from brain_games.scripts.engine import run,welcome_user
+from brain_games.scripts.engine import run, welcome_user
 from brain_games.games import brain_even
 
+
 def main():
-    run(brain_even,welcome_user)
+    run(brain_even, welcome_user)
+
 
 if __name__ == "__main__":
     main()
